b9fcf4e117c2dc83c76023c291deb64cc3a6d297
Guilherme Tavares <62484044+guiTavares13@users.noreply.github.com>
2021-05-24 19:56:55 -0300

    Tela principal e detalhes

--- N2_Ecommerce_adventure/DAO/ConexaoBD.cs
+++ N2_Ecommerce_adventure/DAO/ConexaoBD.cs
@@ -8,10 +8,10 @@ namespace N2_Ecommerce_adventure.DAO
 {
     public class ConexaoBD
     {
-        private static string servername = @"DESKTOP-G71U68M\WINCCPLUSMIG2014";
+        private static string servername = @"NBQSP-FC3495\SQLDEVELOPER2019";
         private static string database = "N2_dudu_Viotti_ecommerce";
         private static string server_login = "sa";
-        private static string server_password = "123456";
+        private static string server_password = "1camisa*";
         public static SqlConnection GetConexao()
         {
             string strCon = $"Data Source={servername};Initial Catalog={database};User ID={server_login};Password={server_password}";
